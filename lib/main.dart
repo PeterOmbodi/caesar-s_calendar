089f40e3af4c0d2520237bc43bf852991885f53a
Peter Ombodi <peter.ombodi@gmail.com>
2025-07-18 18:12:42 +0300

    add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,27 @@
 import 'package:caesar_puzzle/presentation/pages/puzzle/puzzle_screen.dart';
+import 'package:caesar_puzzle/presentation/theme/colors.dart';
 import 'package:caesar_puzzle/presentation/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-
-import 'injection.dart';
-import 'generated/l10n.dart';
-import 'package:caesar_puzzle/presentation/theme/colors.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:provider/provider.dart';
 
+import 'generated/l10n.dart';
+import 'injection.dart';
+
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   configureInjection();
   final themeNotifier = ThemeModeNotifier();
   await themeNotifier.load();
   runApp(
     ChangeNotifierProvider(
       create: (_) => themeNotifier,
-      child: const MyApp(),
+      child: Builder(builder: (context) {
+        FlutterNativeSplash.remove();
+        return const MyApp();
+      }),
     ),
   );
 }
